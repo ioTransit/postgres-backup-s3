1361bdc296c649b5590a849a9a9b19e0888007cf
Walter Jenkins <walter@transit.chat>
2024-10-18 13:41:11 -0500

    update to rm pgclient

--- src/install.sh
+++ src/install.sh
@@ -1,19 +1,23 @@
-#! /bin/sh
+#!/bin/sh
 
 set -eux
 set -o pipefail
 
 apk update
 
-# install pg_dump
+# Remove any existing PostgreSQL client to prevent conflicts
+apk del postgresql-client || true
+
+# Install pg_dump version 16
 apk add postgresql16-client
 
-# install gpg
+# Install gpg
 apk add gnupg
 
+# Install aws-cli
 apk add aws-cli
 
-# install go-cron
+# Install go-cron
 apk add curl
 curl -L https://github.com/ivoronin/go-cron/releases/download/v0.0.5/go-cron_0.0.5_linux_${TARGETARCH}.tar.gz -O
 tar xvf go-cron_0.0.5_linux_${TARGETARCH}.tar.gz
@@ -22,5 +26,5 @@ mv go-cron /usr/local/bin/go-cron
 chmod u+x /usr/local/bin/go-cron
 apk del curl
 
-# cleanup
+# Cleanup
 rm -rf /var/cache/apk/*
